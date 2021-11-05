ed8090f537615e78a165a42f04d06e99cd329dc1
Matheus <matheuspsilva29@gmail.com>
2021-11-05 09:46:43 -0300

    Criando formulario de cadastro

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <Banner/>
   <div class="main-container">
     <h1>Monte o seu burguer:</h1>
-    <p>Formulário</p>
+    <burguer-form/>
   </div>
 </template>
 
 <script>
   import Banner from '../components/Banner.vue'
+  import BurguerForm from '../components/BurguerForm.vue'
+
 
   export default{
     name: "Home",
     components: {
-      Banner
+      Banner,
+      BurguerForm
     }
   }
 
